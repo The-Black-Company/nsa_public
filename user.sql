d560b5a68c8115275c77921de1beef01470a28b3
Elania <83014218+Elania-Marvers@users.noreply.github.com>
2022-11-23 14:01:12 +0000

    Update user.sql

--- user.sql
+++ user.sql
@@ -1,3 +1,3 @@
 CREATE USER 'data-backend'@'localhost' IDENTIFIED BY 'EwUrk9Q46';
-GRANT PRIVILEGE ON *.* TO 'data-backend'@'localhost';
+GRANT ALL PRIVILEGE ON *.* TO 'data-backend'@'localhost';
 FLUSH PRIVILEGES;
