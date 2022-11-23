4d66d3ce95526892fb72de0daa8f8d2fcc1f52e9
Elania <83014218+Elania-Marvers@users.noreply.github.com>
2022-11-23 13:38:09 +0000

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+wget https://files.phpmyadmin.net/phpmyadmin.keyring
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz.asc
+
