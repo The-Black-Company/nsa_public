86b290252fbbe19b7313ea3e7c3c570244bf9197
Elania <83014218+Elania-Marvers@users.noreply.github.com>
2022-11-23 13:39:16 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
 wget https://files.phpmyadmin.net/phpmyadmin.keyring
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz.asc
-
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
